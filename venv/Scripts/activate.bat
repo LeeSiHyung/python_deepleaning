eb6aa372b4e30872be1e509e59e8626e800e5aee
LeeSiHyung <dltlgud1324@gmail.com>
2018-03-04 16:21:08 +0900

    최종 환경 commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\bluecreator\PycharmProjects\test\venv"
+set "VIRTUAL_ENV=C:\Users\master\PycharmProjects\test\venv"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
