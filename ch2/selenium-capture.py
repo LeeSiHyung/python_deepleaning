04c6d2b2ae1cd3ecb8e77b528bca3e53fb1fcca6
LeeSiHyung <dltlgud1324@gmail.com>
2018-05-12 14:01:06 +0900

    최종 환경 commit

---
+++ ch2/selenium-capture.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+
+url = "http://www.naver.com/"
+
+# PhantomJS 드라이버 추출하기
+browser = webdriver.PhantomJS()
+# 3초 대기
+browser.implicitly_wait(3)
+# URL 읽어 들이기
+browser.get(url)
+# 화면을 캡처해서 지정하기
+browser.save_screenshot("Website.png")
+# 브라우저 종료하기
+browser.quit()
