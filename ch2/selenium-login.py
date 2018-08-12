3bce7ff106b62d75e677af93b0f1f23535b0618d
dltlg <dltlgud1324@gmail.com>
2018-08-12 20:29:51 +0900

    수정사항 적용

--- ch2/selenium-login.py
+++ ch2/selenium-login.py
@@ -5,8 +5,8 @@ USER = "dltlgud1324"
 PASS = "Lacruz1324!@"
 
 # PhantomJS 드라이버 추출하기
-# browser = webdriver.PhantomJS()
-browser = webdriver.PhantomJS(executable_path='/phantomjs/bin/phantomjs')
+browser = webdriver.PhantomJS()
+# browser = webdriver.PhantomJS(executable_path='/phantomjs/bin/phantomjs')
 browser.implicitly_wait(3)
 
 # 로그인 페이지에 접근하기
@@ -22,16 +22,18 @@ e = browser.find_element_by_id("pw")
 e.clear()
 e.send_keys(PASS)
 
+# browser.save_screenshot("login.png")
+
 # 입력 양식 전송해서 로그인하기
 form = browser.find_element_by_css_selector("input.btn_global[type=submit]")
 form.submit()
 print("로그인 버튼을 클릭합니다,")
 
+
 # 쇼핑 페이지의 데이터 가져오기
 browser.get("https://order.pay.naver.com/home?tabMenu=SHOPPING")
 
-
-browser.save_screenshot("shopping.png")
+# browser.save_screenshot("shopping.png")
 
 # 쇼핑 목록 출력하기
 products = browser.find_elements_by_css_selector(".p_info span")
